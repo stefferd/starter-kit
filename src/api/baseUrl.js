5ea2e5c04d8eee87c1f66cab591e2d0d29707bfa
stefferd <vandenberg.stef@gmail.com>
2017-10-07 13:57:02 +0200

    Added production build and added the option to switch between api and mock api by a querystring parameter

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,14 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParametersByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParametersByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
